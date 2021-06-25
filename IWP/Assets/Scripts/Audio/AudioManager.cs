66b85c6cfb6d776ac486f56283e8de384fdf5f73
Ryainer <Jonathanjiale@gmail.com>
2021-06-25 18:49:04 +0800

    audio stuff

---
+++ IWP/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] soundarray;
+
+    private void Awake()
+    {
+        foreach(Sound indivsound in soundarray)
+        {
+           indivsound.source = gameObject.AddComponent<AudioSource>();
+            indivsound.source.clip = indivsound.clip;
+
+            indivsound.source.volume = indivsound.volume;
+            indivsound.source.pitch = indivsound.pitch;
+        }
+    }
+
+    public void Player(string soundname)
+    {
+       Sound findclip = Array.Find(soundarray, sound => sound.name == soundname);
+        findclip.source.Play();
+    }
+}
