8196802b3432a006bcae454a283468d40394bb6f
Ryainer <Jonathanjiale@gmail.com>
2021-07-08 18:59:56 +0800

    main menu + animation

---
+++ IWP/Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject OptionsMenu;
+
+    public void onStartGamePressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void onOptionsButtonPressed()
+    {
+        if(!OptionsMenu.activeInHierarchy)
+        {
+            OptionsMenu.SetActive(true);
+        }
+    }
+
+    public void onQuitButton()
+    {
+        Application.Quit();
+    }
+}
