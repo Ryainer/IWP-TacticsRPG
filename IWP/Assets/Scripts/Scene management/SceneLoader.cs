3337835681657c846472e2aaecd9c162fa0a1592
Ryainer <Jonathanjiale@gmail.com>
2021-07-09 12:22:25 +0800

    sceneLoader

---
+++ IWP/Assets/Scripts/Scene management/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
